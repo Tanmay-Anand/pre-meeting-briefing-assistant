32a81ae781fa38e372b248ba347e5e82dee0adf4
Tanmay-Anand <tanmayanand2002@gmail.com>
2026-09-17 15:15:02 +0530

    Initial commit

---
+++ backend/leadlens/src/main/java/com/leadlens/LeadlensApplication.java
@@ -0,0 +1,13 @@
+package com.leadlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadlensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadlensApplication.class, args);
+	}
+
+}
---
+++ backend/leadlens/src/test/java/com/leadlens/LeadlensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leadlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadlensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
